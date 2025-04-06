01615f8c46e5b9080f46fe4b79be0724f5faa2e8
Harry_hcx <61307411+Harry-hcx@users.noreply.github.com>
2025-04-06 11:16:05 +0800

    Add files via upload

--- timer.py
+++ timer.py
@@ -4,7 +4,7 @@ font_name = pg.font.match_font("simhei")
 
 
 class Timer:
-    def __init__(self, x=600, y=500):
+    def __init__(self, x=600, y=500, start=0):
         self.m = 0
         self.s = 0
         self.ms = 0.0
@@ -12,6 +12,7 @@ class Timer:
         self.x = x
         self.y = y
         self.font_size = 30
+        self.go(60, start)
 
     def calc(self):
         self.s += self.ms // 1000
@@ -19,11 +20,11 @@ class Timer:
         self.m += self.s // 60
         self.s %= 60
 
-    def go(self, x):
-        self.tick += 1
-        self.ms += 0.304 * x
+    def go(self, x, cnt=1):
+        self.tick += cnt
+        self.ms += 1.0 / x * cnt * 1000
         self.calc()
-        print(self.toString(), self.tick)
+        # print(self.toString(), self.tick)
 
     def toString(self):
         return str(int(self.m)) + ":" + str(int(self.s)) + "." + str(int(self.ms))
